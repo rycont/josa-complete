526042de9f86c657837722fd740ca60d1a926bcc
정한 <35295182+rycont@users.noreply.github.com>
2022-02-06 08:50:33 +0000

    feat: add 와과

--- src/index.ts
+++ src/index.ts
@@ -4,42 +4,47 @@ declare global {
     을를: string;
     이가: string;
     으로: string;
+    와과: string;
   }
 }
 
-const getJongseongIndex = (letter: string) => (letter.charCodeAt(0) - 44032) % 28
+const getJongseongIndex = (letter: string) =>
+  (letter.charCodeAt(0) - 44032) % 28;
 const hasJongseong = (letter: string) => getJongseongIndex(letter) > 0;
 
 export const createJosaFunction = (a: string, b: string) => ({
-  appender: (word: string | String) => `${word}${hasJongseong(word[word.length - 1]) ? a : b}`,
-  getSuffix: (word: string | String) => hasJongseong(word[word.length - 1]) ? a : b
-})
-const addToString = (key: string, getter: (value: string) => string) => Object.defineProperty(String.prototype, key, {
-  get() {
-    return getter(this)
-  }
-})
+  appender: (word: string | String) =>
+    `${word}${hasJongseong(word[word.length - 1]) ? a : b}`,
+  getSuffix: (word: string | String) =>
+    hasJongseong(word[word.length - 1]) ? a : b,
+});
+const addToString = (key: string, getter: (value: string) => string) =>
+  Object.defineProperty(String.prototype, key, {
+    get() {
+      return getter(this);
+    },
+  });
 
-const { appender: subj1, getSuffix: getSubjectiveSuffix1 } = createJosaFunction('은', '는')
-const { appender: obj, getSuffix: getObjectiveSuffix1 } = createJosaFunction('을', '를')
-const { appender: subj2, getSuffix: getSubjectiveSuffix2 } = createJosaFunction('이', '가')
-const { appender: adv1, getSuffix: getAdverbalSuffix1 } = {
+const { appender: subj1, getSuffix: get은는 } = createJosaFunction("은", "는");
+const { appender: obj, getSuffix: get을를 } = createJosaFunction("을", "를");
+const { appender: subj2, getSuffix: get이가 } = createJosaFunction("이", "가");
+const { appender: adv2, getSuffix: get와과 } = createJosaFunction("과", "와");
+
+const { appender: adv1, getSuffix: get으로 } = {
   getSuffix(word: string | String) {
-    return [0, 8].includes(getJongseongIndex(word[word.length - 1])) ? "로" : "으로"
+    return [0, 8].includes(getJongseongIndex(word[word.length - 1]))
+      ? "로"
+      : "으로";
   },
   appender(word: string | String) {
-    return word + getAdverbalSuffix1(word)
-  }
-}
+    return word + get으로(word);
+  },
+};
 
-addToString('은는', subj1)
-addToString('이가', subj2)
-addToString('을를', obj)
-addToString('으로', adv1)
+addToString("은는", subj1);
+addToString("이가", subj2);
+addToString("을를", obj);
+addToString("으로", adv1);
+addToString("와과", adv2);
 
-export {
-  getSubjectiveSuffix1,
-  getSubjectiveSuffix2,
-  getObjectiveSuffix1,
-  getAdverbalSuffix1
-}
+export { get은는, get이가, get을를, get으로, get와과 };
--- test.js
+++ test.js
@@ -1,16 +1,21 @@
-const {
-    getSubjectiveSuffix1, getSubjectiveSuffix2, getAdverbalSuffix1, getObjectiveSuffix1
-} = require('./dist')
+const { get은는, get이가, get으로, get을를, get와과 } = require("./dist");
 
-test("종이 is 종이를", () => expect("종이".을를).toBe("종이를"))
-test("장미칼 is 장미칼을", () => expect("장미칼".을를).toBe("장미칼을"))
-test("시계 is 시계는", () => expect("시계".은는).toBe("시계는"))
-test("컵 is 컵은", () => expect("컵".은는).toBe("컵은"))
-test("집 is 집으로", () => expect("집".으로).toBe("집으로"))
-test("안정실 is 안정실로", () => expect("안정실".으로).toBe("안정실로"))
-test("NodeJS is NodeJS는", () => expect("NodeJS".은는).toBe("NodeJS는"))
-test("파일 is 파일이", () => expect("파일".이가).toBe("파일이"))
-test("Subjective Suffix of 마우스 is 는", () => expect(getSubjectiveSuffix1("마우스")).toBe("는"))
-test("Subjective Suffix2 of 마우스 is 가", () => expect(getSubjectiveSuffix2("마우스")).toBe("가"))
-test("Adverbal Suffix1 of 마우스 is 로", () => expect(getAdverbalSuffix1("마우스")).toBe("로"))
-test("Objective Suffix of 마우스 is 를", () => expect(getObjectiveSuffix1("마우스")).toBe("를"))
+test("종이 is 종이를", () => expect("종이".을를).toBe("종이를"));
+test("장미칼 is 장미칼을", () => expect("장미칼".을를).toBe("장미칼을"));
+test("시계 is 시계는", () => expect("시계".은는).toBe("시계는"));
+test("컵 is 컵은", () => expect("컵".은는).toBe("컵은"));
+test("집 is 집으로", () => expect("집".으로).toBe("집으로"));
+test("안정실 is 안정실로", () => expect("안정실".으로).toBe("안정실로"));
+test("NodeJS is NodeJS는", () => expect("NodeJS".은는).toBe("NodeJS는"));
+test("조명 is 조명과", () => expect("조명".와과).toBe("조명과"));
+test("파일 is 파일이", () => expect("파일".이가).toBe("파일이"));
+test("Subjective Suffix of 마우스 is 는", () =>
+  expect(get은는("마우스")).toBe("는"));
+test("Subjective Suffix2 of 마우스 is 가", () =>
+  expect(get이가("마우스")).toBe("가"));
+test("Adverbal Suffix1 of 마우스 is 로", () =>
+  expect(get으로("마우스")).toBe("로"));
+test("Objective Suffix of 마우스 is 를", () =>
+  expect(get을를("마우스")).toBe("를"));
+test("Objective Suffix2 of 마우스 is 를", () =>
+  expect(get와과("마우스")).toBe("와"));
