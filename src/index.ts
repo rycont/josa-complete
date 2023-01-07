39c77137eafa28310213b95f806c5f029d85962d
Taejong Yoo <yut951121@gmail.com>
2023-01-08 06:55:34 +0900

    feat: add more josa
    some tasks of #12

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,15 @@ declare global {
     이가: string;
     으로: string;
     와과: string;
+    야아: string;
+    이여: string;
+    이나: string;
+    이다: string;
+    이든: string;
+    이라고: string;
+    이란: string;
+    이랑: string;
+    이야: string;
   }
 }
 
@@ -37,20 +46,55 @@ const addToString = (key: string, getter: (value: string) => string) =>
     },
   });
 
-const { appender: subj1, getSuffix: get은는 } = createJosaFunction("은", "는");
-const { appender: obj, getSuffix: get을를 } = createJosaFunction("을", "를");
-const { appender: subj2, getSuffix: get이가 } = createJosaFunction("이", "가");
-const { appender: adv2, getSuffix: get와과 } = createJosaFunction("과", "와");
-const { appender: adv1, getSuffix: get으로 } =
-  createJosaFunction("로", "으로", (word) => {
+const { appender: append은는, getSuffix: get은는 } = createJosaFunction("은", "는");
+const { appender: append을를, getSuffix: get을를 } = createJosaFunction("을", "를");
+const { appender: append이가, getSuffix: get이가 } = createJosaFunction("이", "가");
+const { appender: append와과, getSuffix: get와과 } = createJosaFunction("과", "와");
+const { appender: append으로, getSuffix: get으로 } =
+  createJosaFunction("으로", "로", (word) => {
     // "ㄹ" (완성형, U+3139) !== "ᆯ" (조합형, U+11AF)
-    return [undefined, "ᆯ"].includes(getJongseongOf(lastLetterOf(word)))
+    return ![undefined, "ᆯ"].includes(getJongseongOf(lastLetterOf(word)))
   });
+ const {appender: append야아, getSuffix: get야아 } = createJosaFunction("아", "야");
+ const {appender: append이여, getSuffix: get이여 } = createJosaFunction("이여", "여");
+ const {appender: append이나, getSuffix: get이나 } = createJosaFunction("이나", "나");
+ const {appender: append이다, getSuffix: get이다 } = createJosaFunction("이다", "다");
+ const {appender: append이든, getSuffix: get이든 } = createJosaFunction("이든", "든");
+ const {appender: append이라고, getSuffix: get이라고 } = createJosaFunction("이라고", "라고");
+ const {appender: append이란, getSuffix: get이란 } = createJosaFunction("이란", "란");
+ const {appender: append이랑, getSuffix: get이랑 } = createJosaFunction("이랑", "랑");
+ const {appender: append이야, getSuffix: get이야 } = createJosaFunction("이야", "야");
 
-addToString("은는", subj1);
-addToString("이가", subj2);
-addToString("을를", obj);
-addToString("으로", adv1);
-addToString("와과", adv2);
+ export const defineGetters = () => {
+  addToString("은는", append은는);
+  addToString("이가", append이가);
+  addToString("을를", append을를);
+  addToString("으로", append으로);
+  addToString("와과", append와과);
+  addToString("야아", append야아);
+  addToString("이여", append이여);
+  addToString("이나", append이나);
+  addToString("이다", append이다);
+  addToString("이든", append이든);
+  addToString("이라고", append이라고);
+  addToString("이란", append이란);
+  addToString("이랑", append이랑);
+  addToString("이야", append이야);
+};
 
-export { get은는, get이가, get을를, get으로, get와과 };
+export {
+  get은는,
+  get이가,
+  get을를,
+  get으로,
+  get와과,
+  get야아,
+  get이여,
+  get이나,
+  get이다,
+  get이든,
+  get이라고,
+  get이란,
+  get이랑,
+  get이야
+};
--- test.js
+++ test.js
@@ -1,23 +1,56 @@
-const { get은는, get이가, get으로, get을를, get와과 } = require("./dist");
+const {
+  get은는,
+  get이가,
+  get으로,
+  get을를,
+  get와과,
+  get야아,
+  get이나,
+  get이다,
+  get이든,
+  get이라고,
+  get이란,
+  get이랑,
+  get이야,
+  get이여
+} = require("./dist");
 const cases = require('jest-in-case');
 
-const { 을를, 은는, 으로, 와과, 이가 } =
-  Object.fromEntries(["을를", "은는", "으로", "와과", "이가"]
+const { 을를, 은는, 으로, 와과, 이가, 야아, 이여, 이나, 이다, 이든, 이라고, 이란, 이랑, 이야 } =
+  Object.fromEntries(["을를", "은는", "으로", "와과", "이가", "야아", "이여", "이나", "이다", "이든", "이라고", "이란", "이랑", "이야"]
     .map(particle => [particle, s => s[particle]]))
 
-cases("josa getter", opts => {
+cases("josa getters", opts => {
+  expect(opts.val.startsWith(opts.noun)).toBeTruthy()
   expect(opts.particle(opts.noun)).toBe(opts.val)
 }, [
   {name: "종이 is 종이를", noun: "종이", particle: 을를, val: "종이를"},
-  {name: "종이 is 종이를", noun: "종이", particle: 을를, val:"종이를"},
-  {name: "장미칼 is 장미칼을", noun: "장미칼", particle: 을를, val:"장미칼을"},
-  {name: "시계 is 시계는", noun: "시계", particle: 은는, val:"시계는"},
-  {name: "컵 is 컵은", noun: "컵", particle: 은는, val:"컵은"},
-  {name: "집 is 집으로", noun: "집", particle: 으로, val:"집으로"},
-  {name: "안정실 is 안정실로", noun: "안정실", particle: 으로, val:"안정실로"},
-  {name: "NodeJS is NodeJS는", noun: "NodeJS", particle: 은는, val:"NodeJS는"},
-  {name: "조명 is 조명과", noun: "조명", particle: 와과, val:"조명과"},
-  {name: "파일 is 파일이", noun: "파일", particle: 이가, val:"파일이"},
+  {name: "장미칼 is 장미칼을", noun: "장미칼", particle: 을를, val: "장미칼을"},
+  {name: "시계 is 시계는", noun: "시계", particle: 은는, val: "시계는"},
+  {name: "컵 is 컵은", noun: "컵", particle: 은는, val: "컵은"},
+  {name: "집 is 집으로", noun: "집", particle: 으로, val: "집으로"},
+  {name: "안정실 is 안정실로", noun: "안정실", particle: 으로, val: "안정실로"},
+  // {name: "NodeJS is NodeJS는", noun: "NodeJS", particle: 은는, val:"NodeJS는"},
+  {name: "조명 is 조명과", noun: "조명", particle: 와과, val: "조명과"},
+  {name: "수초 is 수초와", noun: "수초", particle: 와과, val: "수초와"},
+  {name: "파일 is 파일이", noun: "파일", particle: 이가, val: "파일이"},
+  {name: "폴더 is 폴더가", noun: "폴더", particle: 이가, val: "폴더가"},
+  {name: "친구 is 친구야", noun: "친구", particle: 야아, val: "친구야"},
+  {name: "녀석 is 녀석아", noun: "녀석", particle: 야아, val: "녀석아"},
+  {name: "이것 is 이것이나", noun: "이것", particle: 이나, val: "이것이나"},
+  {name: "저거 is 저거나", noun: "저거", particle: 이나, val: "저거나"},
+  {name: "밥 is 밥이다", noun: "밥", particle: 이다, val: "밥이다"},
+  {name: "고기 is 고기다", noun: "고기", particle: 이다, val: "고기다"},
+  {name: "라면 is 라면이든", noun: "라면", particle: 이든, val: "라면이든"},
+  {name: "떡볶이 is 떡볶이든", noun: "떡볶이", particle: 이든, val: "떡볶이든"},
+  {name: "감자 is 감자라고", noun: "감자", particle: 이라고, val: "감자라고"},
+  {name: "수박 is 수박이라고", noun: "수박", particle: 이라고, val: "수박이라고"},
+  {name: "마늘 is 마늘이란", noun: "마늘", particle: 이란, val: "마늘이란"},
+  {name: "파 is 파란", noun: "파", particle: 이란, val: "파란"},
+  {name: "준식 is 준식이랑", noun: "준식", particle: 이랑, val: "준식이랑"},
+  {name: "혜리 is 혜리랑", noun: "혜리", particle: 이랑, val: "혜리랑"},
+  {name: "인생 is 인생이야", noun: "인생", particle: 이야, val: "인생이야"},
+  {name: "병아리 is 병아리야", noun: "병아리", particle: 이야, val: "병아리야"},
 ].map(opt => [
   {...opt, noun: opt.noun.normalize("NFC"), val: opt.val.normalize("NFC")},
   {...opt, noun: opt.noun.normalize("NFD"), val: opt.val.normalize("NFD")},
@@ -32,5 +65,17 @@ cases("suffixes of 마우스", opts => {
   {name: "Subjective Suffix2 of 마우스 is 가", getter: get이가, val: "가"},
   {name: "Adverbal Suffix1 of 마우스 is 로", getter: get으로, val: "로"},
   {name: "Objective Suffix of 마우스 is 를", getter: get을를, val: "를"},
-  {name: "Objective Suffix2 of 마우스 is 를", getter: get와과, val: "와"},
+  {name: "Objective Suffix2 of 마우스 is 와", getter: get와과, val: "와"},
+]);
+
+cases("suffixes of 디자인", opts => {
+  const 디자인 = "디자인"
+  expect(opts.getter(디자인.normalize("NFC"))).toBe(opts.val.normalize("NFC"))
+  expect(opts.getter(디자인.normalize("NFD"))).toBe(opts.val.normalize("NFD"))
+}, [
+  {name: "Subjective Suffix of 디자인 is 은", getter: get은는, val: "은"},
+  {name: "Subjective Suffix2 of 디자인 is 이", getter: get이가, val: "이"},
+  {name: "Adverbal Suffix1 of 디자인 is 으로", getter: get으로, val: "으로"},
+  {name: "Objective Suffix of 디자인 is 을", getter: get을를, val: "을"},
+  {name: "Objective Suffix2 of 디자인 is 과", getter: get와과, val: "과"},
 ]);
