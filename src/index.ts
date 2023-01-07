b02b3b190852db882a556262873a0f559dc04392
Taejong Yoo <yut951121@gmail.com>
2023-01-08 04:08:43 +0900

    feat: pass tests
    resolves #20

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,28 @@ declare global {
   }
 }
 
-const lastLetterOf = (hangul: string) => hangul.normalize("NFC").slice(-1);
+const containsJohab = (word: string) => word.normalize("NFC") !== word
+const lastLetterOf = (word: string) => word.normalize("NFC").slice(-1);
 const getJongseongOf = (letter: string) => letter.normalize("NFD")[2];
 const hasJongseong = (letter: string) => letter.normalize("NFD").length > 2;
 
-export const createJosaFunction = (a: string, b: string) => ({
-  appender: (word: string) =>
-    `${word}${hasJongseong(lastLetterOf(word)) ? a : b}`,
-  getSuffix: (word: string) =>
-    hasJongseong(lastLetterOf(word)) ? a : b,
-});
+const defaultPredicate = (word: string) => hasJongseong(lastLetterOf(word));
+
+export const createJosaFunction = (
+  whenTrue: string,
+  whenFalse: string,
+  predicate: (_: string) => boolean = defaultPredicate
+) => {
+  const getSuffix = (word: string) => {
+    const suffix = predicate(word) ? whenTrue : whenFalse;
+    return containsJohab(word) ? suffix.normalize("NFD") : suffix;
+  }
+  return {
+    appender: (word: string) =>
+      `${word}${getSuffix(word)}`,
+    getSuffix
+  };
+};
 const addToString = (key: string, getter: (value: string) => string) =>
   Object.defineProperty(String.prototype, key, {
     get() {
@@ -29,18 +41,11 @@ const { appender: subj1, getSuffix: get은는 } = createJosaFunction("은", "는
 const { appender: obj, getSuffix: get을를 } = createJosaFunction("을", "를");
 const { appender: subj2, getSuffix: get이가 } = createJosaFunction("이", "가");
 const { appender: adv2, getSuffix: get와과 } = createJosaFunction("과", "와");
-
-const { appender: adv1, getSuffix: get으로 } = {
-  getSuffix(word: string) {
-    // 아래의 리을은 받침으로서 한글 자판으로 칠 수 없다
+const { appender: adv1, getSuffix: get으로 } =
+  createJosaFunction("로", "으로", (word) => {
+    // "ㄹ" (완성형, U+3139) !== "ᆯ" (조합형, U+11AF)
     return [undefined, "ᆯ"].includes(getJongseongOf(lastLetterOf(word)))
-      ? "로"
-      : "으로";
-  },
-  appender(word: string) {
-    return word + get으로(word);
-  },
-};
+  });
 
 addToString("은는", subj1);
 addToString("이가", subj2);
--- test.js
+++ test.js
@@ -1,21 +1,36 @@
 const { get은는, get이가, get으로, get을를, get와과 } = require("./dist");
+const cases = require('jest-in-case');
 
-test("종이 is 종이를", () => expect("종이".을를).toBe("종이를"));
-test("장미칼 is 장미칼을", () => expect("장미칼".을를).toBe("장미칼을"));
-test("시계 is 시계는", () => expect("시계".은는).toBe("시계는"));
-test("컵 is 컵은", () => expect("컵".은는).toBe("컵은"));
-test("집 is 집으로", () => expect("집".으로).toBe("집으로"));
-test("안정실 is 안정실로", () => expect("안정실".으로).toBe("안정실로"));
-test("NodeJS is NodeJS는", () => expect("NodeJS".은는).toBe("NodeJS는"));
-test("조명 is 조명과", () => expect("조명".와과).toBe("조명과"));
-test("파일 is 파일이", () => expect("파일".이가).toBe("파일이"));
-test("Subjective Suffix of 마우스 is 는", () =>
-  expect(get은는("마우스")).toBe("는"));
-test("Subjective Suffix2 of 마우스 is 가", () =>
-  expect(get이가("마우스")).toBe("가"));
-test("Adverbal Suffix1 of 마우스 is 로", () =>
-  expect(get으로("마우스")).toBe("로"));
-test("Objective Suffix of 마우스 is 를", () =>
-  expect(get을를("마우스")).toBe("를"));
-test("Objective Suffix2 of 마우스 is 를", () =>
-  expect(get와과("마우스")).toBe("와"));
+const { 을를, 은는, 으로, 와과, 이가 } =
+  Object.fromEntries(["을를", "은는", "으로", "와과", "이가"]
+    .map(particle => [particle, s => s[particle]]))
+
+cases("josa getter", opts => {
+  expect(opts.particle(opts.noun)).toBe(opts.val)
+}, [
+  {name: "종이 is 종이를", noun: "종이", particle: 을를, val: "종이를"},
+  {name: "종이 is 종이를", noun: "종이", particle: 을를, val:"종이를"},
+  {name: "장미칼 is 장미칼을", noun: "장미칼", particle: 을를, val:"장미칼을"},
+  {name: "시계 is 시계는", noun: "시계", particle: 은는, val:"시계는"},
+  {name: "컵 is 컵은", noun: "컵", particle: 은는, val:"컵은"},
+  {name: "집 is 집으로", noun: "집", particle: 으로, val:"집으로"},
+  {name: "안정실 is 안정실로", noun: "안정실", particle: 으로, val:"안정실로"},
+  {name: "NodeJS is NodeJS는", noun: "NodeJS", particle: 은는, val:"NodeJS는"},
+  {name: "조명 is 조명과", noun: "조명", particle: 와과, val:"조명과"},
+  {name: "파일 is 파일이", noun: "파일", particle: 이가, val:"파일이"},
+].map(opt => [
+  {...opt, noun: opt.noun.normalize("NFC"), val: opt.val.normalize("NFC")},
+  {...opt, noun: opt.noun.normalize("NFD"), val: opt.val.normalize("NFD")},
+]).flat());
+
+cases("suffixes of 마우스", opts => {
+  const 마우스 = "마우스"
+  expect(opts.getter(마우스.normalize("NFC"))).toBe(opts.val.normalize("NFC"))
+  expect(opts.getter(마우스.normalize("NFD"))).toBe(opts.val.normalize("NFD"))
+}, [
+  {name: "Subjective Suffix of 마우스 is 는", getter: get은는, val: "는"},
+  {name: "Subjective Suffix2 of 마우스 is 가", getter: get이가, val: "가"},
+  {name: "Adverbal Suffix1 of 마우스 is 로", getter: get으로, val: "로"},
+  {name: "Objective Suffix of 마우스 is 를", getter: get을를, val: "를"},
+  {name: "Objective Suffix2 of 마우스 is 를", getter: get와과, val: "와"},
+]);
